8d7c03208dd0ead244f53c7b49046cb497cdbcba
4bderahim <abderahimelkridah@gmail.com>
2024-11-23 18:08:10 +0100

    cppp

---
+++ contact.cpp
@@ -0,0 +1,13 @@
+
+#include "cpp.h"
+
+
+class Contact
+{
+    public:
+    std::string contact_name , contact_last_name, contact_nickname, contact_darkest_sec;
+
+};
+
+
+
--- phone_book.cpp
+++ phone_book.cpp
@@ -1,42 +1,77 @@
+// #include "cpp.h"
 #include <iostream>
 #include <vector>
-
-
+#include <string.h>
+class Contact
+{
+    public:
+    std::string contact_name , contact_last_name, contact_nickname, contact_darkest_sec;
+};
 
 class Phonebook
 {
+    // std::vector<std::string> alo;
+    // std::string contact_name , contact_last_name, contact_nickname, contact_darkest_sec;
     public:
-    std::vector<std::string> alo;
+    Contact contacts[8];
     public:
-    void add_contact(std::string contact)
-    {
-        alo.push_back(contact);
-    }
-    public:
-    void search_(int index)
-    {
-        while (1)
+    void add_contact(std::string contact_name,std::string contact_last_name,std::string contact_nickname, std::string contact_darkest_sec, int index)
         {
-            int i;
-            std::cout << "enter contact id:";
-            std::cin >> i;
-            
+            contacts[index].contact_name = contact_name;
+            contacts[index].contact_last_name = contact_last_name;
+            contacts[index].contact_nickname = contact_nickname;
+            contacts[index].contact_darkest_sec = contact_darkest_sec;
         }
-        
-
+    public:
+    void search(int index)
+    {
+        std::cout << contacts[index].contact_name;
+        std::cout << "\n";
     }
-
-
 };
+std::string new_entry(std::string order)
+{
+    std::string new_;
+    using namespace std;
+
+    std::cout << "enter " << order << ":";
+    getline(cin, new_);
+    return (new_);
+}
 int main()
 {
     std::vector<std::string> alo;
-    std::string j;
+    int index_count;
+    index_count = 0;
+    Phonebook phonebook;
+    using namespace std;
     while (1)
     {
-        std::cout << "|||>>>";
-        std::cin >> j;
-        alo.push_back(j);
-        std::cout << "\n" << j << "\t" << alo[1] ;
+        std::string cmd;
+        std::cout << "|>";
+        getline(cin, cmd);
+        if (!cmd.compare("SEARCH"))
+        {
+            int index;
+            int i;
+            std::cout << "enter contact index:";
+            std::cin >> i;
+            if (index > 7 || index < 0)
+                break;
+            phonebook.search(index);
+        }
+        if (!cmd.compare("ADD"))
+        {
+            std::string contact_name , contact_last_name, contact_nickname, contact_darkest_sec;
+            phonebook.contacts[index_count].contact_name = new_entry("contact_name");
+            phonebook.contacts[index_count].contact_last_name = new_entry("contact_last_name");
+            phonebook.contacts[index_count].contact_nickname = new_entry("contact_nickname");
+            phonebook.contacts[index_count].contact_darkest_sec = new_entry("contact_darkest_sec");
+            index_count++;
+            break;
+        }
+        if (!cmd.compare("EXIT"))
+            break;
+        std::cout << "\n" << cmd << "\t" << alo[1];
     }
 }
