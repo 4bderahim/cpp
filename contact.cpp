0d11649cd8b07d670d85d3a3ac7c7b9a7f2820e6
4bderahim <abderahimelkridah@gmail.com>
2024-11-25 23:41:10 +0100

    cppp

--- contact.cpp
+++ contact.cpp
@@ -6,8 +6,4 @@ class Contact
 {
     public:
     std::string contact_name , contact_last_name, contact_nickname, contact_darkest_sec;
-
 };
-
-
-
--- file.cpp
+++ file.cpp
@@ -2,22 +2,61 @@
 #include <iostream>
 
 
+class Account 
+{
+    public:
+    // static int	getNbAccounts( void )
+    // {}
+        
+	void	makeDeposit(int deposit)
+    {
+            _totalNbDeposits++;
+            std::cout << "deposit >>> " << _totalNbDeposits << "<<<<<<\n";
+    }
+	bool	makeWithdrawal(int withdrawal)
+    {
+        return _totalNbWithdrawals++;
+    }
+	int		checkAmount(void) const
+    {
+        return (_totalAmount);
+    }
+	void	displayStatus(void) const
+    {
+    }
+    private:
+        static int	_nbAccounts;
+	    static int	_totalAmount;
+	    static int	_totalNbDeposits;
+	    static int	_totalNbWithdrawals;
+	    static void	_displayTimestamp(void)
+        {
+            std::string str;
+            int i;
+            std::ifstream MyReadFile("19920104_091532.log");
+            while (getline(MyReadFile, str))
+            {
+                i = 0;
+                while (str[i])
+                {
+                    if (str[i] == ' ')
+                        std::cout << "\n";
+                    i++;
+                }
+            }
+        }
+        int				_accountIndex;
+	    int				_amount;
+	    int				_nbDeposits;
+	    int				_nbWithdrawals;
+        Account(int initial_deposit)
+        {
+            _totalAmount = 0;
+        }
+        ~Account(void);
+};
 
-using namespace std;
 int main()
 {
-    string text;
-    string op;
-    ifstream MyReadFile("19920104_091532.log");
-    while (getline(MyReadFile, text))
-        {
-            // cout << text << "\n";
-            break;
-        }
-    int i = 0;
-    while (text[i])
-    {
-        i++;
-    }
-    MyReadFile.close();
+    Account acc;
 }
