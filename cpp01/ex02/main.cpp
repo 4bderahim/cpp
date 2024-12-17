cf1d319790ed409c63749c5966ff0592251d7ab7
4bderahim <abderahimelkridah@gmail.com>
2024-12-17 15:53:45 +0100

    cpp

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+
+    std::string str;
+    std::string *stringPtr;
+    std::string &stringRef = str;
+    str = "HI THIS IS BRAIN";
+    stringPtr = &str;
+    
+    std::cout << "The memory address of the string variable  : " << &str << std::endl;
+    std::cout << "The memory address held by stringPTR       : " << stringPtr <<  std::endl;
+    std::cout << "The memory address held by stringREF       : " << stringRef  << std::endl;
+    
+}
