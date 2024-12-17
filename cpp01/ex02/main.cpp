fe029ecc56339350dfe19a2fbc4d9dc4680612d3
4bderahim <abderahimelkridah@gmail.com>
2024-12-17 17:21:48 +0100

    cpp

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -7,8 +7,8 @@ int main()
     std::string *stringPtr;
     std::string &stringRef = str;
     str = "HI THIS IS BRAIN";
-    stringPtr = &str;
     
+    stringPtr = &str;
     std::cout << "The memory address of the string variable  : " << &str << std::endl;
     std::cout << "The memory address held by stringPTR       : " << stringPtr <<  std::endl;
     std::cout << "The memory address held by stringREF       : " << stringRef  << std::endl;
---
+++ cpp01/ex03/Weapon.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+
+
+
+class Weapon
+{
+    private:
+        std::string type;
+    
+    const std::string& getType();
+    void setType(std::string new_);
+
+};
