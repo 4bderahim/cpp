93b027b92d73798cb28333c8456ecd16ee92914d
4bderahim <abderahimelkridah@gmail.com>
2024-12-19 23:35:57 +0100

    cpp

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 int main(int argc, char **argv)
 {
-    ifstream file;
+    std::string hello;
     if (argc < 4)
-        {
-            std::cout << "arguments error!! " << std::endl;
-        }
-    file.
-    
+    {
+        std::cout << "arguments error!! " << std::endl;
+    }
+    std::ifstream file (argv[1]);
+    if (file.is_open())
+    {
+        file >> hello;
+    }
 
+    std::cout << ">>" << hello >> "<<";
 }
