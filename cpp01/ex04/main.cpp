fb6d8c1b4ee7a983fcf167d3ee46a30efe458e2a
4bderahim <abderahimelkridah@gmail.com>
2024-12-19 23:22:09 +0100

    cpp

---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char **argv)
+{
+    ifstream file;
+    if (argc < 4)
+        {
+            std::cout << "arguments error!! " << std::endl;
+        }
+    file.
+    
+
+}
