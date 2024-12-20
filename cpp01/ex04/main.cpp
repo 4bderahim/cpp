da47af37ebb0861b820478ff5f0076645351cc08
4bderahim <abderahimelkridah@gmail.com>
2024-12-20 23:58:14 +0100

    cpp

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -9,6 +9,8 @@ int replace_(std::string old_fileName,std::string content, std::string to_be_rep
     new__file =  ".replace";
     new__file = old_fileName + ".replace";
     std::ofstream file (new__file);
+    if (file.fail())
+        return (0);
     while (1)
     {
         int index = content.find(to_be_replaced, i+1);
@@ -38,10 +40,20 @@ int main(int argc, char **argv)
         return (1);
     }
     std::ifstream file (argv[1]);
+    if (!file)
+    {
+        std::cout << "[-] file doesn't exist!";
+        return (1);
+    }
     while (file.is_open() && std::getline(file, hello))
         content += hello + "\n"
         ;
     if (!replace_(argv[1],content ,argv[2],  argv[3]))
+    {
+        std::cout << "[-] Unknown error accured!!" << std::endl;
+        file.close();
         return (1);
-    std::cout << hello;
+    }
+    file.close();
+    return (0);
 }
