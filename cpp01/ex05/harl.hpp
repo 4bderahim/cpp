27c785e9be6c664a4c67aac49ef2fb78cdfd864d
4bderahim <abderahimelkridah@gmail.com>
2024-12-25 17:33:40 +0100

    cppp

---
+++ cpp01/ex05/harl.hpp
@@ -0,0 +1,19 @@
+
+
+#pragma once
+
+#include <iostream>
+
+class Harl{
+
+    private:
+        void debug( void );
+        void info( void );
+        void warning( void );
+        void error( void );
+    public:
+        Harl();
+        void (Harl::*levelsStrPrts[4]) ();
+        void complain( std::string level );
+        
+};
