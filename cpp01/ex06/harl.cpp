0822a666b686bcf90a547b2769f4540b1c87fd71
4bderahim <abderahimelkridah@gmail.com>
2024-12-26 20:57:46 +0100

    vpp

--- cpp01/ex06/harl.cpp
+++ cpp01/ex06/harl.cpp
@@ -7,19 +7,9 @@ Harl::Harl()
 {
     // Harl::levelsStrPrts = {&Harl::debug ,  &Harl::error,  &Harl::warning, &Harl::info};
 }
-void Harl::error()
-{
-    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
-}
-
 void Harl::debug()
 {
     std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
-}
-
-void Harl::warning()
-{
-    std::cout << "think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void Harl::info()
@@ -27,10 +17,20 @@ void Harl::info()
     std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
+void Harl::warning()
+{
+    std::cout << "think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+void Harl::error()
+{
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+
 void Harl::complain( std::string level )
 {
-    void (Harl::*levelsStrPrts[4]) () = {&Harl::debug, &Harl::info,    &Harl::warning,  &Harl::error};
-    Harl h;
+    // void (Harl::*levelsStrPrts[4]) () = {&Harl::debug, &Harl::info,    &Harl::warning,  &Harl::error};
+    // Harl h;
     std::string levels[4] = 
     {
         "debug",
@@ -38,15 +38,34 @@ void Harl::complain( std::string level )
         "warning",
         "error",
     };
-
-    for (int i = 0; i < 4; i++)
+    int i;
+    for (i = 0; i < 4; i++)
     {
         if (!levels[i].compare(level))
-            {
-                (this->*levelsStrPrts[i]) ();
-                return ;
-            }
+            break;
     }
-    std::cout << "Undefined complaint!!" << std::endl;
+    switch (i){
+        case 0:
+            this->debug();
+            this->info();
+            this->warning();
+            this->error();
+            break;
+        case 1:
+            this->info();
+            this->warning();
+            this->error();
+            break;
+        case 2:
+            this->warning();
+            this->error();
+            break;
+        case 3:
+            this->error();
+            break;
+        default:
+            std::cout << "Undefined complaint!!" << std::endl;
+            break;
+    }
 }
 
--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -5,13 +5,13 @@
 int main()
 {
     Harl ha;
-    std::cout  << "WARNING LEVEL:" << std::endl;
+    std::cout  << "WARNING LEVEL AND ABOVE:" << std::endl;
     ha.complain("warning");
-    std::cout  << "\nERROR LEVEL:" << std::endl;
+    std::cout  << "\nERROR LEVEL AND ABOVE:" << std::endl;
     ha.complain("error");
-    std::cout  << "\nDEBUG LEVEL:" << std::endl;
+    std::cout  << "\nDEBUG LEVEL AND ABOVE:" << std::endl;
     ha.complain("debug");
-    std::cout  << "\nERROR LEVEL:" << std::endl;
+    std::cout  << "\nERROR LEVEL AND ABOVE:" << std::endl;
     ha.complain("error");
     return (0);
 }
