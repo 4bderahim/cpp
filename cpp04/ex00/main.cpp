dc51db805da93cfc6c00101d8ba003aebd2024d4
4bderahim <abderahimelkridah@gmail.com>
2025-02-27 17:55:42 +0000

    cpp

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -2,18 +2,22 @@
 #include "Dog.hpp"
 #include "Cat.hpp"
 
+
+
+
 int main()
 {
-const Animal* meta = new Animal();
-const Animal* j = new Dog();
-const Animal* i = new Cat();
-std::cout << meta->getType() << std::endl;
-std::cout << i->getType() << " " << std::endl;
-i->makeSound();
-j->makeSound(); 
-meta->makeSound();
-// delete meta;
-// delete i;
-delete i;
-return 0;
+    const Animal* meta = new Animal();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+    std::cout << meta->getType() << std::endl;
+    std::cout << i->getType() << " " << std::endl;
+    meta->makeSound();
+    i->makeSound();
+    j->makeSound();
+    // delete meta;
+    // delete i;
+    std::system("leaks -q poly");
+    // delete i;
+    return 0;
 }
