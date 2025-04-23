2765d53168fab533980769c810fec0374f76ebc6
4bderahim <abderahimelkridah@gmail.com>
2025-04-23 18:28:45 +0100

    cpp

--- cpp05/ex01/main.cpp
+++ cpp05/ex01/main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    // grade too high
+    // exception catched !
     Bureaucrat b = Bureaucrat("dsdsd", 12);
     Form f = Form(11,84);
     try
@@ -14,23 +14,20 @@ int main()
     {
         std::cout << ee.what() << std::endl;
     }
+
+    // all valid (no exception)
+    Bureaucrat b = Bureaucrat("name", 1);
+    Form f = Form(11,84);
+    try
+    {
+        b.signForm(f);
+    }
+    catch (std::exception &ee)
+    {
+        std::cout << ee.what() << std::endl;
+    }
+
+
     
-    // try
-    // {
-    //     Bureaucrat b = Bureaucrat("dsdsd", 10000);
-    // }   
-    // catch(std::exception &e )
-    // {
-    //     std::cout << e.what() << std::endl;
-    // }
-    // // grade too low
-    // try
-    // {
-    //     Bureaucrat b = Bureaucrat("dsdsd", 0);
-    // }
-    // catch(std::exception &e)
-    // {
-    //     std::cout << e.what() << std::endl;
-    // }
 
 }
