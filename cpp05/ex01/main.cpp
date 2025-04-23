e19ec2fab0eb1ce398b7cfe9101e548a0397e38c
4bderahim <abderahimelkridah@gmail.com>
2025-04-23 23:41:58 +0100

    cpp

--- cpp05/ex01/main.cpp
+++ cpp05/ex01/main.cpp
@@ -3,6 +3,7 @@
 
 int main()
 {
+    {
     // exception catched !
     Bureaucrat b = Bureaucrat("dsdsd", 12);
     Form f = Form(11,84);
@@ -14,20 +15,19 @@ int main()
     {
         std::cout << ee.what() << std::endl;
     }
-
+    }
     // all valid (no exception)
+    {
+    std::cout << "--------------" << std::endl;
     Bureaucrat bb = Bureaucrat("name", 1);
     Form ff = Form(11,84);
     try
     {
-        b.signForm(ff);
+        bb.signForm(ff);
     }
     catch (std::exception &ee)
     {
         std::cout << ee.what() << std::endl;
     }
-
-
-    
-
+    }
 }
