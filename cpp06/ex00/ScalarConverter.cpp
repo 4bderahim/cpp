f7eb70e670863066b32bdfee336333c241365686
4bderahim <abderahimelkridah@gmail.com>
2025-05-07 21:46:50 +0100

    cpp

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -1,13 +1,48 @@
 #include "ScalarConverter.hpp"
 
 
-int to_int(std::string s)
+
+
+int valid(std::string s, std::string s_)
 {
-    int i = 0;
-    while (s[i] == '+' || s[i] == '-')
+    for (int i = 0; i < s.length(); i++)        
     {
+        if (s_.find(s[i]) == std::string::npos)
+            return (0);
         i++;
     }
+    return (1);
+}
+
+int who_am_i(std::string s)
+{
+    // 0 > -inff
+    // 1 > +inff
+    // 2 > char 
+    // 3 > int 
+    // 4 > float
+    // 5 > double
+    int i;
+    i = 0;
+    if (s == "-inff")
+        return i;
+    i++;
+    if (s == "+inff")
+        return i;
+    i++;
+    if (s.length() == 1)
+        return i;
+    i++;
+    if (s.find_last_not_of("01234564789-+") == std::string::npos)
+        return (i);
+    i++;
+    if (s.find_first_not_of("01234564789-+.f") == std::string::npos)
+        return (i);
+    i++;
+    if (s.find_first_not_of("01234564789-+.") == std::string::npos)
+        return (i);
+    return (i);
+
 }
 
 void ScalarConverter::convert(std::string str)
@@ -16,5 +51,6 @@ void ScalarConverter::convert(std::string str)
     float to_float;
     double to_double;
     std::string l;
+
     // l.find_first_not_of()
 }
