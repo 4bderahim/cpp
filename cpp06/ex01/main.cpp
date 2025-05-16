ec87a5163038fb7ee1505c9034e1d691948a7ee7
4bderahim <abderahimelkridah@gmail.com>
2025-05-16 22:49:42 +0100

    cp

--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -3,10 +3,10 @@
 
 int main()
 {
-
     Data data;
     serialize_ ser;
-    data.data_string  = "str";
-    data.data_int =   1234;
+    data.data_string = "str";
+    data.data_int = 1234;
     ser.deserialize(ser.serialize(&data));
 }
+
---
+++ cpp06/ex02/main.cpp
@@ -0,0 +1,26 @@
+
+
+
+
+
+#include <cstdlib> 
+
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base * generate(void)
+{
+
+    int random;
+
+    random = (rand()%3)+1;
+    if (random == 1)
+        return ();
+
+}
+
+int main()
+{
+
+}
