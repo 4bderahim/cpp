839f98053088880b10309260724386a20b8fe980
4bderahim <abderahimelkridah@gmail.com>
2025-05-17 23:38:00 +0100

    cpp

--- cpp07/ex00/template.cpp
+++ cpp07/ex00/template.cpp
@@ -3,6 +3,30 @@
 
 
 template <typename T> 
+T min(T x, T y)
+{
+    if (x < y)
+        return (x);
+    return (y)
+}
+
+template <typename T> 
+
+void swap (T &first, T &sec)
+{
+    T tmp  = *sec;
+
+    *sec = *first;
+    *first = tmp;
+    //
+    
+}
 
 
-//
+template <typename T> 
+T min(T x, T y)
+{
+    if (x > y)
+        return (x);
+    return (y)
+}
