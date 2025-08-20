47e9dce1ec84af3cd3f9def83e38f5b2dc1dbb13
abderahim <4bderahim@github.com>
2025-08-20 18:43:28 +0100

    cpp

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -6,6 +6,13 @@
 
 
 
+
+
+
+
+
+
+
 int main(int argc , char **argv)
 {
     if (argc != 2)
@@ -13,20 +20,20 @@ int main(int argc , char **argv)
         std::cout << "args error !" << std::endl;
         return (1);
     }
+    // std::cout << argv[1] << std::endl;
     std::ifstream f(argv[1]);
     if (!f.is_open())
     {
         std::cout << "error opening the file!" << std::endl;
         return(1);
     }
-    std::string str;
-
-    while (std::getline(f, str))    
+     std::string str;
+    //  str <<  f.rdbuf();
+    while (getline(f, str))    
         {
             // reading file >>> str..
         }
     f.close();
-    
     
     return (1);
 }
