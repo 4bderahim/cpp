26c89cb7f748add93e260308a3c6284501f453e8
abderahim <4bderahim@github.com>
2025-08-20 17:35:49 +0100

    cpp

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -5,6 +5,7 @@
 // #include "BitcoinExchange.hpp"
 
 
+
 int main(int argc , char **argv)
 {
     if (argc != 2)
@@ -12,14 +13,20 @@ int main(int argc , char **argv)
         std::cout << "args error !" << std::endl;
         return (1);
     }
-
     std::ifstream f(argv[1]);
-
     if (!f.is_open())
     {
         std::cout << "error opening the file!" << std::endl;
         return(1);
     }
+    std::string str;
+
+    while (std::getline(f, str))    
+        {
+            // reading file >>> str..
+        }
+    f.close();
+    
     
     return (1);
 }
