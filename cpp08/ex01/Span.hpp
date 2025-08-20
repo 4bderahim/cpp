b84cd10f501c6bc508fc25182f140e219ac7b685
abderahim <4bderahim@github.com>
2025-08-20 16:39:27 +0100

    cpp

--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -14,6 +14,7 @@ class maxsize : public std::exception
         }
 };
 
+
 class Span
 {
     private:
---
+++ cpp09/ex00/main.cpp
@@ -0,0 +1,16 @@
+
+
+
+#include "BitcoinExchange.hpp"
+
+int main(int argc , char **argv)
+{
+    if (argc != 2)
+    {
+        std::cout << "args error !" << std::endl;
+        return (1);
+    }
+    
+
+    return (1);
+}
