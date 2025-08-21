7ad8bc622dcc69adeb59bdf313d074efacc416ef
abderahim <4bderahim@github.com>
2025-08-21 19:23:13 +0100

    cp

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -51,6 +51,7 @@ int main(int argc , char **argv)
                 std::cout << "file Error!!" << std::endl;
                 return (1);
             }
+            i++;
 
         }
                 }
