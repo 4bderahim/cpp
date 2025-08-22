462248fde40bd5dd3868d41c5f835ab7a50ce47d
abderahim <4bderahim@github.com>
2025-08-22 01:33:45 +0100

    cpp

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -8,19 +8,26 @@
 
 int check_data(std::string str)
 {
+
+    // printf("\t\t\t####");
     std::string tmp = str;
     if (std::count(str.begin(), str.end(), '|') !=1)
         return (0);
     tmp = str.substr(0, str.find('|'));
+    // printf("%s>", tmp.c_str());
+
     if (std::count(tmp.begin(), tmp.end(), '-') !=2)
         return (0);
     tmp.erase(std::remove(tmp.begin(), tmp.end(), ' '), tmp.end());
-    tmp.erase(std::remove(tmp.begin(), tmp.end(), '|'), tmp.end());
-    tmp.erase(std::remove(tmp.begin(), tmp.end(), '-'), tmp.end());
     
-
-
-
+    tmp.erase(std::remove(tmp.begin(), tmp.end(), '-'), tmp.end());
+    printf("%s?", tmp.c_str());
+    if (!std::atoi(tmp.c_str()))
+        return (0);
+    // tmp.erase(std::remove(tmp.begin(), tmp.end(), ' '), tmp.end());
+    // tmp.erase(std::remove(tmp.begin(), tmp.end(), '|'), tmp.end());
+    // tmp.erase(std::remove(tmp.begin(), tmp.end(), '-'), tmp.end());
+    return (1);
 }
 
 int check_first_line(std::string& str)
@@ -52,26 +59,35 @@ int main(int argc , char **argv)
      int i = 0;
     //  str <<  f.rdbuf();
     while (getline(f, str)) 
-        {
-            // std::cout << "|" << str<< "|" <<  std::endl;
+    {
+            // std::cout << str << std::endl;
             if ((!i && str.size()) && str.find_first_not_of(" ") != std::string::npos)
-        {
-            i=1;
-            // continue;
-            
-        }
-        
-        if (i == 1)
-        {
-            if (!check_first_line(str))
             {
-                std::cout << "file Error!!" << std::endl;
-                return (1);
+                i=1;
+                // continue;
+                
             }
-            i++;
-
-        }
+            
+            if (i ==1)
+            {
+                if (i == 1&& !check_first_line(str))
+                {
+                    std::cout << "file Error!!" << std::endl;
+                    return (1);
                 }
+                else
+                {
+                    // std::cout << "d|" << str<< "|" <<  std::endl;
+                    i = 99;
+                    
+                    continue;
+                }
+                i++;
+                
+            }
+            if (!check_data(str))
+                {}// printf("\t{######}]\n");
+        }
     f.close();
     
     return (1);
