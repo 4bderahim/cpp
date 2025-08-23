0020c3836329760f7f41c7d5c33d5bdf9ccf8ea8
abderahim <4bderahim@github.com>
2025-08-23 17:09:02 +0100

    cpp

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -12,7 +12,7 @@ int that_error(std::string error_msg)
 
     return (0);
 }
-int check_file_data(std::string str)
+int check_file_data(std::string str, std::map<std::string , double> map)
 {
     std::string value, date;
     std::string tmp = str;
@@ -48,8 +48,12 @@ int check_file_data(std::string str)
     tmp = tmp.substr(tmp.find('|')+1,  tmp.length()-1);
     if (std::atoi(tmp.c_str()) > 1000 || std::atoi(tmp.c_str()) < 0)
         return (that_error("[-] value error "+str));
+    
+    // if (map.find(date) == map.end())
+    //     return 29;//
     value = tmp ;
-    std::cout << date << "|" << value << std::endl;
+    std::cout << date << "=>" << value << "=>" << map[date]*(std::atof(value.c_str())) << std::endl;
+    // std::cout << date << "|" << value << std::endl;?
     return (1);
 }
 
@@ -58,7 +62,7 @@ int check_first_line(std::string& str)
     str.erase(std::remove(str.begin(), str.end(), ' '), str.end());
     if (str != "date|value")
         {
-            printf("[%s]", str.c_str());
+            // printf("[%s]", str.c_str());
             return 0;
         }
     return (2);
@@ -69,20 +73,15 @@ int check_first_line(std::string& str)
 void  map_data(std::map<std::string , double> &data, std::string file)
 {
 
-
     std::fstream f(file);
     std::string each_line;
     // no checks for database ,,..file should be perfect!
     while (getline(f, each_line))
-{
+    {
         data[each_line.substr(0, each_line.find(","))] = std::atof(each_line.substr( each_line.find(",")+1, each_line.length() ).c_str())  ;
 
-}
-     for (std::map<std::string, double>::const_iterator it = data.begin(); it != data.end(); ++it) {
-        std::cout << "Key: " << it->first << ", Value: " << it->second << std::endl;
     }
-
-
+    
 
 }
 
@@ -132,7 +131,7 @@ int main(int argc , char **argv)
                 i++;
                 
             }
-            if (!check_file_data(str))
+            if (!check_file_data(str, data))
                 {}// printf("\t{######}]\n");
         }
     f.close();
