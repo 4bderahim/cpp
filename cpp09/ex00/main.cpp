3007660a72b298177a78da6041553fbe09603c0d
abderahim <4bderahim@github.com>
2025-08-23 16:06:16 +0100

    cpp

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+ #include <iostream>
 #include <fstream>
 #include <string>
 #include <algorithm>
@@ -13,6 +13,7 @@ int that_error(std::string error_msg)
 }
 int check_file_data(std::string str)
 {
+    std::string value, date;
     std::string tmp = str;
     if (std::count(str.begin(), str.end(), '|') !=1)
         return (0);
@@ -20,13 +21,16 @@ int check_file_data(std::string str)
     if (std::count(tmp.begin(), tmp.end(), '-') !=2)
         return (0);
     tmp.erase(std::remove(tmp.begin(), tmp.end(), ' '), tmp.end());
+    date = tmp ;
     tmp.erase(std::remove(tmp.begin(), tmp.end(), '-'), tmp.end());
+    
     if (tmp.find_last_not_of("0987654321") != std::string::npos)
     {
         std::cout << "[-] BAD input >>" << str << std::endl;
         return (0);
     }
     tmp =  str.substr(0, str.substr(0, str.find('|')).find('-')) ;
+    
     if (tmp.length() > 4 || std::atoi(tmp.c_str() ) > 2025)
     {
         std::cout << "[-] BIG year number >> " << str << std::endl;
@@ -35,18 +39,15 @@ int check_file_data(std::string str)
     tmp =  str;
     tmp.erase(std::remove(tmp.begin(), tmp.end(), ' '), tmp.end());
     tmp =  tmp.substr(tmp.find('-')+1,  tmp.find('|')) ;
-    printf("%s<<<<\n", tmp.c_str());
     if (std::atoi(tmp.substr(0,  tmp.find('-')).c_str()) > 12)
         return (that_error("[-] month number error "+str));
     if (std::atoi(tmp.substr(tmp.find('-')+1,  tmp.find('|')).c_str()) > 31)
         return (that_error("[-] day error "+str));
+    
     tmp = tmp.substr(tmp.find('|')+1,  tmp.length()-1);
     if (std::atoi(tmp.c_str()) > 1000 || std::atoi(tmp.c_str()) < 0)
         return (that_error("[-] value error "+str));
-    
-    // tmp.erase(std::remove(tmp.begin(), tmp.end(), ' '), tmp.end());
-    // tmp.erase(std::remove(tmp.begin(), tmp.end(), '|'), tmp.end());
-    // tmp.erase(std::remove(tmp.begin(), tmp.end(), '-'), tmp.end());
+    value = tmp ;
     return (1);
 }
 
