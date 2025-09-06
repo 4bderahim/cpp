1ba6f0a8ba01cfa1e15846eda9e0167b3b4f66af
abderahim <4bderahim@github.com>
2025-09-06 15:12:33 +0100

    cpp

--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -1,18 +1,51 @@
 
-
 #include <iostream>
+#include <unistd.h>
+#include <limits>
+int valid(std::string str)
+{
+    if (str[0] == '+')
+        str.erase(0,1);
+    if (str.find_last_not_of("0987654312") != std::string::npos)
+        return (0);
+    if(std::atol(str.c_str()) > UINT32_MAX)
+        return (0);
+
+    return (1)// allgoood
+    
+
+}
 
 
-// int valid()
+
+int this_error(std::string str)
+{
+
+
+    std::cout << str << std::endl;
+    return (1);
+
+}
+
 
 
 int main(int argc ,  char **argv)
 {
-
+    if (argc == 1 || argc == 2 )
+    {
+        std::cout << "args Error" << std::endl;
+        return (1);
+    }
+    for (size_t i = 0; i < argc; i++)
+    {
+        if (!valid(argv[i]))
+            return (this_error("args error"));
+        
+    }
     
 
 
-    
+
 
     return (0);
 }
