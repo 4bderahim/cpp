165658d98496ca8339017e573f795d09368fbd04
abderahim <4bderahim@github.com>
2025-09-06 17:49:21 +0100

    cpp

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -15,5 +15,8 @@ class mergeme
 
 
     private:
-        
+        std::vector<std::pair< unsigned int,unsigned int>> pairs;
+        std::vector<unsigned int> bigs;
+        std::vector<unsigned int> smalls;
+
 }
