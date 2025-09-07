72642f9112272a73850c6fe28bd5b57f052c1823
abderahim <4bderahim@github.com>
2025-09-07 01:19:45 +0100

    cpp

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <limits>
     #include <vector>
+    #include <deque>
 
 
 
@@ -11,25 +12,33 @@
 class mergeme
 {
 
-
-
-
     private:
         // std::vector<std::pair< unsigned int,unsigned int>> pairs;
-        std::vector<unsigned int> bigs;
-        std::vector<unsigned int> smalls;
-        std::vector<unsigned int> final;
+        
         unsigned int lonely;
     public:
-        void pairing(int args, char **argv)
+        void pairing(int args, char **argv, int vector)
         {
+        //    if (vector)
+        //    {
+
+            std::vector<unsigned int> bigs;
+            std::vector<unsigned int> smalls;
+            std::vector<unsigned int> final;
+           if (vector)
+           {
+                std::deque<unsigned int> bigs;
+                std::deque<unsigned int> smalls;
+                std::deque<unsigned int> final;
+
+           }
             int i = 1;
             args--;
             if (args %2 !=0)
-                {
-                    lonely = std::atol(argv[0]);
-                    i == 2;
-                }
+            {
+                lonely = std::atol(argv[0]);
+                i == 2;
+            }
             while (i < args)
             {
                 unsigned int first = std::atol(argv[i]);
@@ -37,10 +46,9 @@ class mergeme
                 bigs.push_back((first < sec ? sec:first));
                 smalls.push_back((first < sec ? first:sec));
                
-                i+=2;
+                i += 2;
             }
-            // for (const auto& p : pairs) {
-            //     std::cout << "(" << p.first << ", " << p.second << ")" << std::endl;
+            
     }
 
         }
