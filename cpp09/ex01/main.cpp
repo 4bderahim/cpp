d73744b8119f9c268c4faeb32168a2050e9e0c76
4bderahim <abderahimelkridah@gmail.com>
2025-10-07 20:51:39 +0100

    cpp

--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -4,31 +4,35 @@
 int precedence(std::string f, std::string s )
 {
     std::string pre = "*/-+" ;
+    std::cout << f << "|" << s << std::endl;
     if (pre.find_first_of(f)  < 2 && pre.find_first_of(s) > 1)
         return (1);
+    printf("-1");
     return (-1);
 
 }
- void fill_stack(std::string operands, std::stack<std::string>  lifo_operators, std::string str)
+ void fill_stack(std::string &operands, std::stack<std::string>  lifo_operators, std::string str)
  {
     // int i = str.length();
     std::string nums = "0987654321";
-    for (int i =  str.length(); i >= 0; i--)
+    // for (int i =  str.length(); i >= 0; i--)
+    for (size_t i = 0; i < str.length(); i++)
     {
         if ( nums.find_first_of(str[i]) != std::string::npos)
         {
-            operands +=  (str.substr(i, 1))+" ";
+            operands +=  (str.substr(i, 1));
             // lifo.push(std::atoi(str.substr(i, 1).c_str()));
         }
         else
         {
-            if (str[i] ==' ')
-                continue;
+            printf("\t|%s|\n", str.substr(i, 1).c_str());
+            // if (str[i] == ' ')
+            //     continue;
             if (lifo_operators.size() ==0)
                 lifo_operators.push(str.substr(i, 1).c_str());   
             else
             {
-                if (precedence(str.substr(i, 1).c_str(), lifo_operators.top()))
+                if (precedence(str.substr(i, 1), lifo_operators.top()) == 1)
                     lifo_operators.push(str.substr(i, 1).c_str()); 
                 else
                 {
@@ -56,27 +60,30 @@ int main(int argc , char **argv)
     }
     
     // std::stack<std::string > lifo_operands;
-    std::string oprands;
+    std::string operands;
     std::stack<std::string > lifo_operators;
 
     fill_stack(operands, lifo_operators ,str);  
-    if (((std::count(str.begin(), str.end(), '-') 
-    +std::count(str.begin(), str.end(), '+') 
-    +std::count(str.begin(), str.end(), '*') 
-    +std::count(str.begin(), str.end(), '/')   ) != (long int)lifo.size()-1) || lifo.size() == 1)
-    {
-        std::cout << "OPERATION ERROR!" << std::endl;
-        return (1);
-    }
-    std::size_t index;
-    while (( index = str.find_first_of("0987654321")) != std::string::npos)
-        {
-            str.erase(index , 1);
-        }
-    str.erase(std::remove(str.begin(), str.end(), ' '), str.end());
-    // printf("\t\t\t>>>>%s]", str.c_str());
-    rpn rpn;
-    rpn.rpnn(lifo, str);
+
+    printf("\t[%s]", operands.c_str());
+
+    // if (((std::count(str.begin(), str.end(), '-') 
+    // +std::count(str.begin(), str.end(), '+') 
+    // +std::count(str.begin(), str.end(), '*') 
+    // +std::count(str.begin(), str.end(), '/')   ) != (long int)lifo.size()-1) || lifo.size() == 1)
+    // {
+    //     std::cout << "OPERATION ERROR!" << std::endl;
+    //     return (1);
+    // }
+    // std::size_t index;
+    // while (( index = str.find_first_of("0987654321")) != std::string::npos)
+    //     {
+    //         str.erase(index , 1);
+    //     }
+    // str.erase(std::remove(str.begin(), str.end(), ' '), str.end());
+    // // printf("\t\t\t>>>>%s]", str.c_str());
+    // rpn rpn;
+    // rpn.rpnn(lifo, str);
     
     
 
