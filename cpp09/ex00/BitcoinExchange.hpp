7f367a8c95f0f4991835f91cb77362a52ff57fef
4bderahim <abderahimelkridah@gmail.com>
2025-10-12 19:58:17 +0100

    cpp

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -47,12 +47,39 @@ class BitcoinExchange
             std::string value, date;
             std::string tmp = str;
             // printf("\t[%s]", str.c_str());
-            if (std::count(str.begin(), str.end(), '|') !=1)
-                return (0);
+            
+            if (tmp.find_first_of("0987654321-|") == std::string::npos)
+                return 0;
+            if (std::count(str.begin(), str.end(), '|') !=1 || std::count(str.begin(), str.end(), '.') !=1)
+                {
+                    return (that_error( "[-] BAD input >>"+str));
+                    return (0);
+                }
             tmp = str.substr(0, str.find('|'));
             if (std::count(tmp.begin(), tmp.end(), '-') !=2)
-                return (0);
+                {
+                    return (that_error( "[-] BAD input >>"+str));
+                    return (0);
+                }
+            
+            
             tmp.erase(std::remove(tmp.begin(), tmp.end(), ' '), tmp.end());
+
+
+
+
+
+            
+            size_t i= tmp.find('-');
+            i++;
+            std::string s = tmp.substr(i, 2);
+            if (s[1] == '-')
+                {
+                    tmp.insert(tmp.find('-')+1,1, '0');
+                }
+            if(tmp.substr(tmp.find_last_of("-")+1 ,2).size() == 1)
+                tmp.insert(tmp.find_last_of("-")+1,1, '0');
+
             date = tmp;
             tmp.erase(std::remove(tmp.begin(), tmp.end(), '-'), tmp.end());
             if (tmp.find_last_not_of("0987654321") != std::string::npos)
@@ -62,25 +89,23 @@ class BitcoinExchange
                 return (that_error("[-] BIG year number >> "+str));
             tmp =  str;
             tmp.erase(std::remove(tmp.begin(), tmp.end(), ' '), tmp.end());
-            tmp =  tmp.substr(tmp.find('-')+1,  tmp.find('|')) ;
-            if (std::atoi(tmp.substr(0,  tmp.find('-')).c_str()) > 12 || std::atoi(tmp.substr(0,  tmp.find('-')).c_str()) <= 0)
+            tmp =  tmp.substr(tmp.find('-')+1, tmp.find('|')) ;
+            if (std::atoi(tmp.substr(0, tmp.find('-')).c_str()) > 12 || std::atoi(tmp.substr(0,  tmp.find('-')).c_str()) <= 0)
                 return (that_error("[-] month number error "+str));
-            if (std::atoi(tmp.substr(tmp.find('-')+1,  tmp.find('|')).c_str()) > 31 || std::atoi(tmp.substr(tmp.find('-')+1,  tmp.find('|')).c_str()) <= 0)
+            if (std::atoi(tmp.substr(tmp.find('-')+1,  tmp.find('|')).c_str()) > 31 || std::atoi(tmp.substr(tmp.find('-')+1, tmp.find('|')).c_str()) <= 0)
                 return (that_error("[-] day error "+str));
             if (std::atoi(tmp.substr(0,  tmp.find('-')).c_str()) == 2 &&  std::atoi(tmp.substr(tmp.find('-')+1,  tmp.find('|')).c_str()) > 29)
                 return (that_error("[-] day error "+str));
-            tmp = tmp.substr(tmp.find('|')+1,  tmp.length()-1);
+            tmp = str.substr(str.find('|')+1,  str.length()-1);
             if (std::atoi(tmp.c_str()) > 1000 || std::atoi(tmp.c_str()) < 0)
                 return (that_error("[-] value error "+str));
             if (map.find(date) == map.end())
                 {
-                    // printf("\t\t\t\t++%s++\n", date.c_str());
                     int l = 300000000;
                     std::string date_to_use;
                     std::map<std::string, double>::const_iterator prv;
                      for (std::map<std::string, double>::const_iterator it = map.begin(); it != map.end(); ++it) {
                     {
-
                         date.erase(std::remove(date.begin(),date.end(), '-'), date.end());
                         int date_ = std::atoi(date.c_str());
 
@@ -97,13 +122,15 @@ class BitcoinExchange
                             }
                     }
                     if (l == 300000000)
+                        {
+                            printf("\t\t#####%s######\n", str.c_str());
                         return (29);
+                        }
                 }
                 date = date_to_use;
-                // std::cout << "\t\t[---" << date << "---]"  << std::endl;
                 }
             value = tmp ;
-            std::cout << date << "=>" << value << "=>" << map[date]*(std::atof(value.c_str())) << std::endl;
+            std::cout << date << "=>" << std::atof(value.c_str()) << "=>" << map[date]*(std::atof(value.c_str())) << std::endl;
             
             return (1);
         }
